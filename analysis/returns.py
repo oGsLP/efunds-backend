62a89294d63b5c10f388973a450773905f02c3b1
oGsLP <1145234011@qq.com>
2021-08-10 17:01:53 +0800

    [analysis] 修改累计收益率的实现

--- analysis/returns.py
+++ analysis/returns.py
@@ -11,18 +11,25 @@
 from .base import const, decimal_divide, decimal_add, decimal_minus
 
 
-def cal_cumulative_return_rate(data) -> str:
-    if type(data) is tuple:
-        return str(decimal_minus(decimal_divide(data[1], data[0], const.PREC), 1))
-    if type(data) is list:
-        data.sort(key=lambda item: item["date"])
-        print(data[0])
-        print(data[-1])
-        return \
-            str(decimal_minus(
+def cal_cumulative_return_rate(data: tuple) -> str:
+    return str(decimal_minus(decimal_divide(data[1], data[0], const.PREC), 1))
+
+
+def cal_cumulative_return_rate_list(data: list) -> list[dict]:
+    data.sort(key=lambda item: item["date"])
+    result = []
+    first = data[0]
+    for i in range(1, len(data)):
+        cur = data[i]
+        result.append({
+            "date": cur["date"],
+            "car": str(decimal_minus(
                 decimal_divide(
-                    decimal_add(data[-1]["cumulative_return_rate"], 1, const.PREC),
-                    decimal_add(data[0]["cumulative_return_rate"], 1, const.PREC)), 1))
+                    decimal_add(cur["cumulative_return_rate"], 1),
+                    decimal_add(first["cumulative_return_rate"], 1), const.PREC), 1))
+        })
+
+    return result
 
 
 def cal_cumulative_abnormal_return(data):
