149c15951b1a526f0faead805ace01470b1b2dde
oGsLP <1145234011@qq.com>
2021-08-15 16:02:12 +0800

    [api] 补充完善api接口

--- api.py
+++ api.py
@@ -8,12 +8,14 @@
 " @time: 2021/8/4 22:16
 " @function: 
 """
+
 from flask import Blueprint, request
 
-api = Blueprint("api", __name__)
-
+from constants import Const
 from service import service_factory
-from util.api_util import succeed
+from util.api_util import succeed, fail
+
+api = Blueprint("api", __name__)
 
 
 @api.route("/test")
@@ -22,31 +24,52 @@ def test():
     return succeed(data_service.get_raw_data(110022))
 
 
-@api.route("/data/<string:code>")
+@api.route("/data/<int:code>")
 def get_data(code):
-    return parse_args_and_get_data(code)
+    return response_data(parse_args_and_get_data(code)[1])
 
 
-@api.route("/data/<string:code>/returns")
+@api.route("/data/<int:code>/returns")
 def get_returns_data(code):
     analysis_service = service_factory.analysis_service
+    data = parse_args_and_get_data(code)[1]
+    if not data or len(data) == 0:
+        return fail(desc="None data crawled!")
+    return succeed(analysis_service.get_returns_data(data))
 
-    return succeed(analysis_service.get_returns_data(parse_args_and_get_data(code)))
 
-
-@api.route("/data/<string:code>/risk")
+@api.route("/data/<int:code>/risk")
 def get_risk_data(code):
     analysis_service = service_factory.analysis_service
+    is_range, data = parse_args_and_get_data(code)[1]
+    if not data or len(data) == 0:
+        return fail(desc="None data crawled!")
+    if is_range:
+        return response_data(analysis_service.get_risk_data_range(data))
+    return response_data(analysis_service.get_risk_data(data))
 
-    return succeed(analysis_service.get_risk_data(parse_args_and_get_data(code)))
 
-
-def parse_args_and_get_data(code):
+def parse_args_and_get_data(code: int) -> tuple[bool, list]:
     data_service = service_factory.data_service
-    from_date = request.args.get("from")
-    to_date = request.args.get("to")
+    range_type = request.args.get("type")
 
-    if to_date and to_date:
-        data = data_service.get_raw_data_with_range(code, from_date, to_date)
+    if not range_type or range_type == Const.req.RANGE_ALL:
+        return False, data_service.get_raw_data(code)
+    if range_type == Const.req.RANGE_CY:
+        return True, data_service.get_raw_data_current_year(code)
+    if range_type == Const.req.RANGE_PERIOD:
+        from_date = request.args.get("from")
+        to_date = request.args.get("to")
+        if from_date and to_date:
+            return True, data_service.get_raw_data_with_range(code, from_date, to_date)
+        else:
+            return False, data_service.get_raw_data(code)
     else:
-        data = data_service.get_raw_data(code)
+        return False, []
+
+
+def response_data(data):
+    if data:
+        return succeed(data)
+    else:
+        return fail(desc="Analyzed data is none!")
